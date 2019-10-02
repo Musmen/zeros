20324b349e00a11d3acb30181f211ddf7aa23afe
Igor Musmen <musmen@tut.by>
2019-10-02 23:49:28 +0200

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+
 module.exports = function zeros(expression) {
-  // your solution
+  var twoCounter = 0;
+  var fiveCounter = 0; 
+
+  var getStep = function(element) {
+    return (element.indexOf('!!') == -1) ? 1 : 2;
+  }
+
+  var getValue = function(element) {
+    return parseInt(element, 10);
+  }
+
+  var countTwoFiveMultipliers  = function(factorialMultiplier) {
+    while (true) {
+      if (factorialMultiplier % 5 == 0) {
+        fiveCounter++;
+        factorialMultiplier /= 5;
+      } else if (factorialMultiplier % 2 == 0) {
+        twoCounter++;
+        factorialMultiplier /= 2;
+      } else break;
+    }
+  };
+
+  var checkFactorialMultipliers = function (factorialMaxMultiplier, increment) {
+    for (var i = factorialMaxMultiplier; i > 0;) {
+      countTwoFiveMultipliers(i);
+      i -= increment;
+    }
+  }
+  
+  // *** item :  ['5!']  ['7!']  ['6!!'] ...  
+  var countZeros = function(item) {
+    var step = getStep(item);
+    var value = getValue(item);
+    checkFactorialMultipliers(value, step);
+  }
+
+  // *** expression : ('5!*7!*6!!*7!!')
+  expression.split('*').forEach(countZeros);
+
+  return Math.min(twoCounter, fiveCounter);
 }
